dd233acf76cb6ec7117e872b0cad11d6a9807e89
MurodillaErgashev03 <ergashevmurodilla27@gmail.com>
2025-12-02 16:41:00 +0500

    router page togrilandi

--- src/provider/router/index.tsx
+++ src/provider/router/index.tsx
@@ -1,19 +1,23 @@
 import { useEffect } from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 import { changeTheme } from 'src/app/slices/layoutSlice';
 import { useAppDispatch, useTypedSelector } from 'src/app/store';
 
-// Layouts
 import MainLayout from 'src/components/layouts/main';
 import { STORAGE_KEYS } from 'src/constants/storage';
 
-// Pages
 import { Custom404Page, HelpPage, HomePage } from 'src/pages';
 import LoginPage from 'src/pages/login/login';
 
 function RouterProvider() {
   const dispatch = useAppDispatch();
   const { isAuthenticated } = useTypedSelector((state) => state.auth);
+
   useEffect(() => {
     const theme = localStorage.getItem(STORAGE_KEYS.THEME) ?? 'light';
     dispatch(changeTheme(theme));
@@ -22,7 +26,6 @@ function RouterProvider() {
   return (
     <Router>
       <Routes>
-        {/* Help */}
         <Route path="/help" element={<HelpPage />} />
 
         {isAuthenticated ? (
@@ -33,7 +36,10 @@ function RouterProvider() {
           <Route path="/login" element={<LoginPage />} />
         )}
 
-        {/* Auth layout */}
+        {!isAuthenticated && (
+          <Route path="/" element={<Navigate to="/login" replace />} />
+        )}
+
         <Route path="/auth" element={<MainLayout />}>
           <Route path="signup" element={<Custom404Page />} />
         </Route>
